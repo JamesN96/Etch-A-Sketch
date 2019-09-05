fac64566a6fd644bde66dbc3a31929a7ce31fbc2
James Nguyen <james.nguyenn96@gmail.com>
2019-09-05 15:13:13 +1000

    Change width and height of divs created in loops to 1fr

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let container = document.getElementById("container");
 for(let i = 0; i < 4; i++) {
     for(let j = 0; j < 4; j++) {
         let div = document.createElement("div");
-        div.style.width = "100px";
-        div.style.height = "100px";
+        div.style.width = "1fr";
+        div.style.height = "1fr";
         div.style.border = "1px solid green";
         container.appendChild(div);
     }
