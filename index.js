08cb353cbaabe484ed12dfc4d7c4dc1dc2941e12
James Nguyen <james.nguyenn96@gmail.com>
2019-09-05 15:37:11 +1000

    Add a line in nested loop to give all grid squares a class attribute. Add selector for all grid squares. Add a loop for grid squares to add two event listeners in loop

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ for(let i = 0; i < 4; i++) {
     for(let j = 0; j < 4; j++) {
         let div = document.createElement("div");
         div.style.width = "1fr";
-        div.style.height = "1fr";
+        div.style.height = "1fr ";
         div.style.border = "1px solid green";
+        div.className = "grid";
         container.appendChild(div);
     }
 };
+
+
+let grids = document.querySelectorAll(".grid");
+
+grids.forEach(function(grid){
+    grid.addEventListener("mouseover", function(e){
+        grid.style.backgroundColor = "red";
+    })
+    grid.addEventListener("mouseout", function(e){
+        grid.style.backgroundColor = "white";
+    })
+})
